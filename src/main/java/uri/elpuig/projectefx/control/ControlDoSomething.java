9392743b0f7014b0acdc75a1802a6ff578320f3f
uriloop <orlopez@elpuig.xeill.net>
2022-01-23 23:38:22 +0100

    Provant d'afegir el fxml i que es conecti no tira

--- src/main/java/uri/elpuig/projectefx/control/ControlDoSomething.java
+++ src/main/java/uri/elpuig/projectefx/control/ControlDoSomething.java
@@ -56,68 +56,15 @@ public class ControlDoSomething implements Initializable {
                 "Tría", "Education", "Recreational","Social","DIY","Charity","Cooking","Relaxation","Music","Busywork")
         );
 
-
-
         participants= new TextField();
         dificultat.setMin(0);
         dificultat.setMax(1);
-
         gratis= new CheckBox();
 
 
-        StringBuilder sb= new StringBuilder();
-
-
-        if (buscar.isPressed()) {
-
-            if (gratis.isSelected()) sb.append("activity?price=0.0&");
-            if(!participants.getText().equals("")) sb.append("activity?participants="+participants.getText()+"&");
-            sb.append("activity?minaccessibility=0&maxaccessibility="+dificultat.getValue()+"&");
-            switch (tipus.getValue().toString()){
-                case "Tría":
-                    break;
-                case "Education":
-                    sb.append("activity?type=education&");
-                    break;
-                case "Recreational":
-                    sb.append("activity?type=recreational&");
-                    break;
-                case "Social":
-                    sb.append("activity?type=social&");
-                    break;
-                case "DIY":
-                    sb.append("activity?type=diy&");
-                    break;
-                case "Charity":
-                    sb.append("activity?type=charity&");
-                    break;
-                case "Cooking":
-                    sb.append("activity?type=cooking&");
-                    break;
-                case "Relaxation":
-                    sb.append("activity?type=relaxation&");
-                    break;
-                case "Music":
-                    sb.append("activity?type=music&");
-                    break;
-                case "Busywork":
-                    sb.append("activity?type=busywork&");
-                    break;
-            }
-            Connection connect= new Connection();
-            DoSomething activity= connect.getRandomActivityToDo(sb.toString());
-            activitat.setText(activity.getActivity());
-
-
-
-        }
 
         if (reiniciar.isPressed()){
-            dificultat.setValue(0);
-            tipus.setValue("Tría");
-            gratis.setSelected(false);
-            participants.setText("");
-            activitat.setText("");
+
         }
        /* if (buscar.isPressed()){
 
@@ -125,7 +72,59 @@ public class ControlDoSomething implements Initializable {
         }*/
 
     }
-    public void buscar(StringBuilder sb){
+
+    private void reiniciar(){
+        dificultat.setValue(0);
+        tipus.setValue("Tría");
+        gratis.setSelected(false);
+        participants.setText("");
+/*
+        activitat.setText("");
+*/
+    }
+
+
+
+
+    private void buscar(){
+        StringBuilder sb= new StringBuilder();
+        if (gratis.isSelected()) sb.append("activity?price=0.0&");
+        if(!participants.getText().equals("")) sb.append("activity?participants="+participants.getText()+"&");
+        sb.append("activity?minaccessibility=0&maxaccessibility="+dificultat.getValue()+"&");
+        switch (tipus.getValue().toString()){
+            case "Tría":
+                break;
+            case "Education":
+                sb.append("activity?type=education&");
+                break;
+            case "Recreational":
+                sb.append("activity?type=recreational&");
+                break;
+            case "Social":
+                sb.append("activity?type=social&");
+                break;
+            case "DIY":
+                sb.append("activity?type=diy&");
+                break;
+            case "Charity":
+                sb.append("activity?type=charity&");
+                break;
+            case "Cooking":
+                sb.append("activity?type=cooking&");
+                break;
+            case "Relaxation":
+                sb.append("activity?type=relaxation&");
+                break;
+            case "Music":
+                sb.append("activity?type=music&");
+                break;
+            case "Busywork":
+                sb.append("activity?type=busywork&");
+                break;
+        }
+        Connection connect= new Connection();
+        DoSomething activity= connect.getRandomActivityToDo(sb.toString());
+        activitat.setText(activity.getActivity());
 
     }
 
